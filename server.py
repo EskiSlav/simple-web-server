0b4bd7f9f3747f47b8b0e21051fa826d9bb9ac18
EskiSlav <uberbug544@gmail.com>
2022-08-17 14:00:29 +0300

    The server that just answer

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
+from audioop import add
 from os import system
 import socket
 
-sock = socket.socket()
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+serversocket.bind(("localhost", 8080))
+# become a server socket
+serversocket.listen(5)
 
-stop = False
+while True:
+    # accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    # now do something with the clientsocket
+    # in this case, we'll pretend this is a threaded server
+    data = clientsocket.recv(2048)
+    clientsocket.send(f"data received: \'{data}\' from address: {address}".encode())
+    clientsocket.close()
 
-
-    
